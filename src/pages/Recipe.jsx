bb8c0cd2f4cba49ba7462b7cca2313de7ed9de72
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-29 18:32:27 +0200

    _

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,10 +1,46 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export const Recipe = () => {
 
     const recipe = useParams().recipe;
+    const navigate = useNavigate();
+
+    const [recipeInfo, setRecipeInfo] = useState({});
+
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipe}`)
+            .then(res => res.json())
+            .then(data => setRecipeInfo(data.meals[0]))
+    }, []);
+
+    console.log(recipeInfo)
 
     return (
-        <h1>Recipe: {recipe}</h1>
+        <>
+            <div className='d-flex justify-content-between'>
+                <h1>Recipe: {recipeInfo.strMeal}</h1>
+                <button onClick={() => navigate(-1)}>Go back</button>
+            </div>
+            <div className='d-flex justify-content-between mt-5'>
+                <div>
+                    <div>
+                        <h6>
+                            Area: {recipeInfo.strArea}
+                        </h6>
+                    </div>
+                    <div>
+                        <h6>
+                            Category: {recipeInfo.strCategory}
+                        </h6>
+                    </div>
+                    <p className='mt-3' style={{ maxWidth: '80%' }}>{recipeInfo.strInstructions}</p>
+                </div>
+                <div>
+                    <img className='recipe-icon' src={recipeInfo.strMealThumb} alt="Recipe" />
+                    <a href={recipeInfo.strYoutube}>Watch <b>{recipeInfo.strMeal}</b> on Youtube</a>
+                </div>
+            </div>
+        </>
     );
 };
