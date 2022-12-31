3e2e24e70f8cf8b3cce7355f2c20475e8a57aaf4
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-31 22:23:35 +0200

    _

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,8 +1,6 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const RecipeItem = (props) => {
-
-    const navigate = useNavigate();
 
     return (
         <article className='col-lg-3 col-md-6 col-sm-12 d-flex justify-content-center'>
@@ -10,7 +8,9 @@ export const RecipeItem = (props) => {
                 <img src={props.img} alt="Recipe" />
                 <h3>{props.title}</h3>
                 <div className='d-flex align-items-end' style={{ height: '100%' }}>
-                    <button onClick={() => navigate(`/recipe/${props.navigateTo}`)}>See more</button>
+                    <Link to={'/recipe/' + props.navigateTo}>
+                        <button>See more</button>
+                    </Link>
                 </div>
             </div>
         </article>
