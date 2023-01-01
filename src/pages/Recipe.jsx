12302f7f47e91275abd7ec5f74554e5687a0ffe4
Danylo Diachenko <danildiachenko23@gmail.com>
2023-01-01 17:39:46 +0200

    _

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -14,6 +14,109 @@ export const Recipe = () => {
             .then(data => setRecipeInfo(data.meals[0]))
     }, []);
 
+    const ingredients = [
+        {
+            id: 1,
+            ingredient: recipeInfo.strIngredient1,
+            measure: recipeInfo.strIngredient1,
+        },
+        {
+            id: 2,
+            ingredient: recipeInfo.strIngredient2,
+            measure: recipeInfo.strIngredient2,
+        },
+        {
+            id: 3,
+            ingredient: recipeInfo.strIngredient3,
+            measure: recipeInfo.strIngredient3,
+        },
+        {
+            id: 4,
+            ingredient: recipeInfo.strIngredient4,
+            measure: recipeInfo.strIngredient4,
+        },
+        {
+            id: 5,
+            ingredient: recipeInfo.strIngredient5,
+            measure: recipeInfo.strIngredient5,
+        },
+        {
+            id: 6,
+            ingredient: recipeInfo.strIngredient6,
+            measure: recipeInfo.strIngredient6,
+        },
+        {
+            id: 7,
+            ingredient: recipeInfo.strIngredient7,
+            measure: recipeInfo.strIngredient7,
+        },
+        {
+            id: 8,
+            ingredient: recipeInfo.strIngredient8,
+            measure: recipeInfo.strIngredient8,
+        },
+        {
+            id: 9,
+            ingredient: recipeInfo.strIngredient9,
+            measure: recipeInfo.strIngredient9,
+        },
+        {
+            id: 10,
+            ingredient: recipeInfo.strIngredient10,
+            measure: recipeInfo.strIngredient10,
+        },
+        {
+            id: 11,
+            ingredient: recipeInfo.strIngredient11,
+            measure: recipeInfo.strIngredient11,
+        },
+        {
+            id: 12,
+            ingredient: recipeInfo.strIngredient12,
+            measure: recipeInfo.strIngredient12,
+        },
+        {
+            id: 13,
+            ingredient: recipeInfo.strIngredient13,
+            measure: recipeInfo.strIngredient13,
+        },
+        {
+            id: 14,
+            ingredient: recipeInfo.strIngredient14,
+            measure: recipeInfo.strIngredient14,
+        },
+        {
+            id: 15,
+            ingredient: recipeInfo.strIngredient15,
+            measure: recipeInfo.strIngredient15,
+        },
+        {
+            id: 16,
+            ingredient: recipeInfo.strIngredient16,
+            measure: recipeInfo.strIngredient16,
+        },
+        {
+            id: 17,
+            ingredient: recipeInfo.strIngredient17,
+            measure: recipeInfo.strIngredient17,
+        },
+        {
+            id: 18,
+            ingredient: recipeInfo.strIngredient18,
+            measure: recipeInfo.strIngredient18,
+        },
+        {
+            id: 19,
+            ingredient: recipeInfo.strIngredient19,
+            measure: recipeInfo.strIngredient19,
+        },
+        {
+            id: 20,
+            ingredient: recipeInfo.strIngredient20,
+            measure: recipeInfo.strIngredient20,
+        },
+    ];
+
     return (
         <>
             <div className='d-flex justify-content-between'>
@@ -22,7 +125,7 @@ export const Recipe = () => {
                 ) : (
                     <h1>Recipe: {recipe}</h1>
                 )}
-                <button onClick={() => navigate(-1)}>Go back</button>
+                <button className="btn-go-back" onClick={() => navigate(-1)}>Go back</button>
             </div>
             {recipeInfo.idMeal ? (
                 <div className='d-flex justify-content-between mt-5'>
@@ -37,7 +140,21 @@ export const Recipe = () => {
                                 Category: {recipeInfo.strCategory}
                             </h6>
                         </div>
-                        <p className='mt-3' style={{ maxWidth: '80%' }}>{recipeInfo.strInstructions}</p>
+                        <h3 className="mt-4">Ingredients:</h3>
+                        <div style={{ maxWidth: '80%' }}>
+                            {ingredients.map((item) => (
+                                <div key={item.id} className="mt-2 d-flex align-items-center">
+                                    {item.ingredient && (
+                                        <>
+                                            <h5>{item.id}) {item.ingredient}</h5>
+                                            <span style={{ margin: '0 5px' }}>-</span>
+                                            <h5>{item.measure}</h5>
+                                        </>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                        <p className='mt-4 recipe-desc' style={{ maxWidth: '80%' }}>{recipeInfo.strInstructions}</p>
                     </div>
                     <div>
                         <img className='recipe-icon' src={recipeInfo.strMealThumb} alt="Recipe" />
